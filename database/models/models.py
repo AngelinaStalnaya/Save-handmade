3acd2ea46956ad53e75103942bafd2e78ed3abe5
Angelina Stalnaya <angelinastalnaya.feb@gmail.com>
2024-03-28 18:32:08 +0300

    basic DB models added

--- database/models/models.py
+++ database/models/models.py
@@ -36,3 +36,34 @@ class BaseModel(peewee.Model):
         legacy_table_names = False
         table_function = make_table_name
         
+class Auth(BaseModel):
+    id = peewee.AutoField()
+    user_id = peewee.IntegerField()
+    token = peewee.TextField()
+    token_lifetime = peewee.TextField()
+      
+class Users(BaseModel):
+    id = peewee.AutoField()
+    user_name = peewee.TextField()
+    login = peewee.TextField()
+    password = peewee.TextField()
+    
+class Sections(BaseModel):
+    id = peewee.AutoField()
+    section_name = peewee.TextField()
+    user_id = peewee.TextField()
+    
+class Subsections(BaseModel):
+    id = peewee.AutoField()
+    section_id = peewee.IntegerField()
+    subsection_name = peewee.TextField()
+    user_id = peewee.TextField()       
+    
+class PatternPage(BaseModel):
+    id = peewee.AutoField()
+    section_id= peewee.IntegerField()
+    subsection_id = peewee.IntegerField(None)
+    pattern_name = peewee.TextField()
+    comments = peewee.TextField()
+    files_ids = peewee.TextField()
+    description = peewee.TextField()
